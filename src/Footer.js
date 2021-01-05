e7c5c79a27e09dd9ddabfcdce60a59d1efcd836a
felipeberger <feconchaber@gmail.com>
2021-01-05 19:52:51 +1100

    Updated footer so it is fixed at the bottom of the screen as well as some minor changes to how the information displays at different screen sizes

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
-  
+
     return (
-      <footer className="bg-dark text-white">
+      <footer className="bg-dark text-white fixed-bottom">
         <div className="container-fluid py-2">
-          <div className="row justify-content-between">
-            <div className="col-md-4 col-sm-5 align-self-center">
+          <div className="row justify-content-md-between justify-content-sm-center">
+            <div className="col-md-5 align-self-center d-none d-md-block">
               <h4>Created by: <br />Felipe Berger</h4>
             </div>
-            <div className="col-md-4 col-sm-5 text-right contact-links">
+            <div className="col-md-4 col-sm-12 text-right contact-links">
               <div className="text-center">
                 <h4>Contact Me:</h4>
                 <a href="https://www.linkedin.com/in/felipe-berger" target="_blank" rel="noopener noreferrer" className="px-3"><i className="fab fa-linkedin"></i></a>
