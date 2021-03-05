a0693646265232539bcebd761da0ca1a6821552a
felipeberger <feconchaber@gmail.com>
2021-03-06 10:44:56 +1100

    Style edits to CSS documents and removal of redundant code in App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,46 @@ import './App.css';
 import Template from './Template.js';
 import SingleExchange from './SingleExchange.js';
 import MultiExchange from './MultiExchange.js';
+import Table from './Table.js';
+import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom";
+
+
+const NotFound = () => {
+  return <h2>404 Not Found</h2>
+}
+
+function Comparison() {
+  return (
+    <>
+      <div className={"content"}>
+      <SingleExchange />
+      <MultiExchange />
+      </div>
+    </>
+  )
+}
+
+const RatesTable = () => {
+  return (
+    <>
+      <Table />
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-      <Template>
-        <SingleExchange />
-        <MultiExchange />
-      </Template>
+        <Router>
+          <Template>
+            <Switch>
+              <Route path="/" exact component={withRouter(Comparison)} />
+              <Route path="/comparison" component={withRouter(Comparison)} />
+              <Route path="/table" component={withRouter(RatesTable)} />           
+              <Route component={NotFound} />
+            </Switch>
+          </Template>
+        </Router>
     </>
   );
 }
