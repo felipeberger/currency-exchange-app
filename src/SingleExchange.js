fbd7700755d36fb0ca857da8441e6ccbf3b63a7b
felipeberger <feconchaber@gmail.com>
2021-03-17 09:56:05 +1100

    Updated fetch URL as previous one broke

--- src/SingleExchange.js
+++ src/SingleExchange.js
@@ -27,7 +27,7 @@ class SingleExchange extends React.Component {
 
     let {base, comparison} = this.state;
 
-    fetch(`https://alt-exchange-rate.herokuapp.com/latest?base=${base}`)
+    fetch(`https://api.exchangeratesapi.io/latest?base=${base}`)
       .then(checkStatus)
       .then(json)
       .then((data) => {
